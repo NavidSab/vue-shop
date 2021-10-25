c80b9ecff8bcdda7775a1f9db1f028394b84cd08
navid <navidsky10@gmail.com>
2021-10-25 14:07:57 +0330

    fix

--- src/main.js
+++ src/main.js
@@ -5,32 +5,26 @@ import "popper.js";
 import "bootstrap";
 import "@/assets/app.scss";
 import jQuery from "jquery";
-import {fb} from "./firebase";
+import { fb } from "./firebase";
+import Swal from 'sweetalert2';
+window.Swal = Swal;
+const Toast = Swal.mixin({
+    toast: true,
+    position: 'top-end',
+    showConfirmButton: false,
+    timer: 3000
+});
 
+window.Toast = Toast;
 const Vue = createApp(App);
-
-window.$ =window.jQuery=jQuery;
-
-
-
-
-
-
-
+window.$ = window.jQuery = jQuery;
 Vue.component("Navbar", require("@/components/Navbar.vue").default);
 Vue.component("Hero", require("@/components/Hero.vue").default);
 Vue.component("ProductList", require("@/sections/ProductList.vue").default);
 Vue.component("Login", require("@/components/Login.vue").default);
-
-
 let app = '';
 fb.auth().onAuthStateChanged(function() {
-
-  if(!app){
-    createApp(App).use(router).mount("#app");
-  }
-
+    if (!app) {
+        createApp(App).use(router).mount("#app");
+    }
 });
-
-
-
--- src/views/Products.vue
+++ src/views/Products.vue
@@ -1,12 +1,10 @@
 <template>
   <div class="products">
       <div class="container">
-          
         <div class="intro h-100">
             <div class="row h-100 justify-content-center align-items-center">
               <div class="col-md-6">
                     <h3>Products Page</h3>
-                    
                  <p>
                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Unde, ducimus.
                  </p>
@@ -16,17 +14,11 @@
               </div>
             </div>
           </div>
-
           <hr>
-
           <div class="product-test">
-
-
             <h3 class="d-inline-block">Products list</h3>
             <button @click="addNew" class="btn btn-primary float-right">Add Product</button>
-
             <div class="table-responsive">
-              
                 <table class="table">
                   <thead>
                     <tr>
@@ -35,33 +27,24 @@
                       <th>Modify</th>
                     </tr>
                   </thead>
-
-                  <!-- <tbody>
-                      <tr v-for="product in products">
+                  <tbody>
+                      <tr v-for="product in products" v-bind:key="product">
                         <td>
                           {{product.name}}
                         </td>
-
                         <td>
                           {{product.price}}
                         </td>
-
                         <td>
-
                           <button class="btn btn-primary" @click="editProduct(product)">Edit</button>
                           <button class="btn btn-danger" @click="deleteProduct(product)">Delete</button>
                         </td>
-
                       </tr>
-
-
-                  </tbody> -->
+                  </tbody>
                 </table>
             </div>
-
           </div>
       </div>
-
       <!-- Modal -->
       <div class="modal fade" id="product" tabindex="-1" role="dialog" aria-labelledby="editLabel" aria-hidden="true">
         <div class="modal-dialog modal-lg" role="document">
@@ -73,14 +56,12 @@
               </button>
             </div>
             <div class="modal-body">
-
                 <div class="row">
                   <!-- main product -->
                   <div class="col-md-8">
                     <div class="form-group">
                       <input type="text" placeholder="Product Name" v-model="product.name" class="form-control">
                     </div>
-
                     <div class="form-group">
                       <!-- <vue-editor v-model="product.description"></vue-editor> -->
                     </div>
@@ -89,28 +70,21 @@
                   <div class="col-md-4">
                     <h4 class="display-6">Product Details</h4>
                     <hr>
-
                     <div class="form-group">
                       <input type="text" placeholder="Product price" v-model="product.price" class="form-control">
                     </div>
-
                     <div class="form-group">
-                      <!-- <input type="text" @keyup.188="addTag" placeholder="Product tags" v-model="tag" class="form-control"> -->
-                      
+                      <!-- <input type="text" v-on:keyup.188="addTag" placeholder="Product tags" v-model="tag" class="form-control"> -->
                       <div  class="d-flex">
                         <!-- <p v-for="tag in product.tags">
                             <span class="p-1">{{tag}}</span>
                         </p> -->
-
                       </div>
                     </div>
-
-
                     <div class="form-group">
                       <label for="product_image">Product Images</label>
                       <input type="file" @change="uploadImage" class="form-control">
                     </div>
-
                     <div class="form-group d-flex">
                       <!-- <div class="p-1" v-for="(image, index) in product.images">
                           <div class="img-wrapp">
@@ -119,13 +93,8 @@
                           </div>
                       </div> -->
                     </div>
-
                   </div>
                 </div>
-
-
-
-
             </div>
             <div class="modal-footer">
               <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
@@ -135,18 +104,11 @@
           </div>
         </div>
       </div>
-
-
-    
   </div>
 </template>
-
 <script>
 // import { VueEditor } from "vue2-editor";
-// import { fb, db} from '../firebase';
-
-import {  db} from '../firebase';
-
+import { fb, db} from '../firebase';
 export default {
   name: "Products",
   // components: {
@@ -155,7 +117,6 @@ export default {
   props: {
     msg: String
   },
-
   data(){
     return {
         products: [],
@@ -171,64 +132,46 @@ export default {
         tag: null
     }
   },
-
   firestore(){
       return {
         products: db.collection('products'),
       }
   },
   methods:{
-
-    // deleteImage(img,index){
-
-    //   let image = fb.storage().refFromURL(img);
-
-    //   this.product.images.splice(index,1);
-
-    //   image.delete().then(function() {
-    //     console.log('image deleted');
-    //   }).catch(function(error) {
-    //     // Uh-oh, an error occurred!
-    //     console.log('an error occurred');
-    //   });
-
-    // },
-
+    deleteImage(img,index){
+      let image = fb.storage().refFromURL(img);
+      this.product.images.splice(index,1);
+      image.delete().then(function() {
+        console.log('image deleted');
+      }).catch(function(error) {
+        console.log(error);
+        // Uh-oh, an error occurred!
+        console.log('an error occurred');
+      });
+    },
     addTag(){
        this.product.tags.push(this.tag);
        this.tag = "";
     },
-    // uploadImage(e){
-
-    //   if(e.target.files[0]){
-        
-    //       let file = e.target.files[0];
-    
-    //       var storageRef = fb.storage().ref('products/'+ Math.random() + '_'  + file.name);
-    
-    //       let uploadTask  = storageRef.put(file);
-    
-    //       uploadTask.on('state_changed', (snapshot) => {
-            
-    //       }, (error) => {
-    //         // Handle unsuccessful uploads
-    //       }, () => {
-    //         // Handle successful uploads on complete
-    //         // For instance, get the download URL: https://firebasestorage.googleapis.com/...
-            
-    //         uploadTask.snapshot.ref.getDownloadURL().then((downloadURL) => {
-    //           this.product.images.push(downloadURL);
-    //         });
-
-    //       });
-
-    //   }
-
-
-
-
-    // },
-
+    uploadImage(e){
+      if(e.target.files[0]){
+          let file = e.target.files[0];
+          var storageRef = fb.storage().ref('products/'+ Math.random() + '_'  + file.name);
+          let uploadTask  = storageRef.put(file);
+          uploadTask.on('state_changed', (snapshot) => {
+            console.log(snapshot);
+          }, (error) => {
+            console.log(error);
+            // Handle unsuccessful uploads
+          }, () => {
+            // Handle successful uploads on complete
+            // For instance, get the download URL: https://firebasestorage.googleapis.com/...
+            uploadTask.snapshot.ref.getDownloadURL().then((downloadURL) => {
+              this.product.images.push(downloadURL);
+            });
+          });
+      }
+    },
     reset(){
       this.product = {
           name:null,
@@ -238,86 +181,60 @@ export default {
           images:[]
       }
     },
-
     addNew(){
         this.modal = 'new';
         this.reset();
         $('#product').modal('show');
     },
-    // updateProduct(){
-    //     this.$firestore.products.doc(this.product.id).update(this.product);
-    //       // Toast.fire({
-    //       //   type: 'success',
-    //       //   title: 'Updated  successfully'
-    //       // })
-
-    //        $('#product').modal('hide');
-    // },
-
+    updateProduct(){
+        this.$firestore.products.doc(this.product.id).update(this.product);
+          Toast.fire({
+            type: 'success',
+            title: 'Updated  successfully'
+          })
+           $('#product').modal('hide');
+    },
     editProduct(product){
       this.modal = 'edit';
       this.product = product;
       $('#product').modal('show');
     },
-
-
-    // deleteProduct(doc){
-
-
-      // Swal.fire({
-      //   title: 'Are you sure?',
-      //   text: "You won't be able to revert this!",
-      //   type: 'warning',
-      //   showCancelButton: true,
-      //   confirmButtonColor: '#3085d6',
-      //   cancelButtonColor: '#d33',
-      //   confirmButtonText: 'Yes, delete it!'
-      // }).then((result) => {
-      //   if (result.value) {
-
-      //     this.$firestore.products.doc(doc.id).delete()
-
-      //     // Toast.fire({
-      //     //   type: 'success',
-      //     //   title: 'Deleted  successfully'
-      //     // })
-
-        
-      //   }
-      // })
-
-
-        
-    // },
+    deleteProduct(doc){
+      Swal.fire({
+        title: 'Are you sure?',
+        text: "You won't be able to revert this!",
+        type: 'warning',
+        showCancelButton: true,
+        confirmButtonColor: '#3085d6',
+        cancelButtonColor: '#d33',
+        confirmButtonText: 'Yes, delete it!'
+      }).then((result) => {
+        if (result.value) {
+          this.$firestore.products.doc(doc.id).delete()
+          Toast.fire({
+            type: 'success',
+            title: 'Deleted  successfully'
+          })
+        }
+      })
+    },
     readData(){
-
-      
-     
     },
     addProduct(){
-      
       this.$firestore.products.add(this.product);
-      
-          // Toast.fire({
-          //   type: 'success',
-          //   title: 'Product created successfully'
-          // })
-
+          Toast.fire({
+            type: 'success',
+            title: 'Product created successfully'
+          })
       $('#product').modal('hide');
     }
-
-  
   },
   created(){
-  
-
   }
 };
 </script>
-
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
-
 .img-wrapp{
   position: relative;
 }
@@ -329,5 +246,4 @@ export default {
 .img-wrapp span.delete-img:hover{
   cursor: pointer;
 }
-
 </style>
