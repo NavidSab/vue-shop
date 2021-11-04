59094f6144c66f9ecb78b5ebfb93cd3e113c829b
navid <navidsky10@gmail.com>
2021-11-04 07:36:22 +0330

    fix

--- src/router/index.js
+++ src/router/index.js
@@ -3,60 +3,67 @@ import Home from "@/views/Home.vue";
 import Admin from "@/views/Admin.vue";
 import Product from "@/views/Products.vue";
 import Profile from "@/views/Profile.vue";
-import {fb} from "../firebase";
+import { fb } from "../firebase";
 
-const routes = [
-  {
-    path: "/",
-    name: "Home",
-    component: Home,
-  },
-  {
-    path: "/admin",
-    name: "Admin",
-    meta: { requiresAuth: true },
-    component: Admin,
-    children:[
-      {
-        path: "products",
-        name: "product",
-        component: Product,
-      },
-      {
-        path: "profile",
-        name: "Profile",
-        component: Profile,
-      }
-    ]
+const routes = [{
+        path: "/",
+        name: "Home",
+        component: Home,
+    },
+    {
+        path: "/admin",
+        name: "Admin",
+        meta: { requiresAuth: true },
+        component: Admin,
+        children: [{
+                path: "products",
+                name: "product",
+                component: Product,
+            },
+            {
+                path: "profile",
+                name: "Profile",
+                component: Profile,
+            }
+        ]
     },
 
-  {
-    path: "/about",
-    name: "About",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue"),
-  },
+    {
+        path: "/about",
+        name: "About",
+        // route level code-splitting
+        // this generates a separate chunk (about.[hash].js) for this route
+        // which is lazy-loaded when the route is visited.
+        component: () =>
+            import ( /* webpackChunkName: "about" */ "../views/About.vue"),
+    },
+    // {
+    //     path: "/checkout",
+    //     name: "checkout",
+    //     // route level code-splitting
+    //     // this generates a separate chunk (about.[hash].js) for this route
+    //     // which is lazy-loaded when the route is visited.
+    //     component: () =>
+    //         import ( /* webpackChunkName: "about" */ "../views/Checkout.vue")
+    // },
 ];
 
 const router = createRouter({
-  history: createWebHistory(process.env.BASE_URL),
-  routes,
+    history: createWebHistory(process.env.BASE_URL),
+    routes,
 });
 
 
 router.beforeEach((to, from, next) => {
-  const requiresAuth = to.matched.some(x => x.meta.requiresAuth)
-  const currentUser = fb.auth().currentUser
-  if (requiresAuth && !currentUser) {
-      next('/')
-  } else if (requiresAuth && currentUser) {
-      next()
-  } else {
-      next()
-  }
+    const requiresAuth = to.matched.some(x => x.meta.requiresAuth)
+    const currentUser = fb.auth().currentUser
+    if (requiresAuth && !currentUser) {
+        next('/')
+    } else if (requiresAuth && currentUser) {
+        next()
+    } else {
+        next()
+    }
 })
 
 
