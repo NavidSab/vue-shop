fed6d0be28f7e31775c38cd4228243c15c2b4b3e
navid <navidsky10@gmail.com>
2021-11-08 18:18:28 +0330

    fix

--- src/router/index.js
+++ src/router/index.js
@@ -37,15 +37,15 @@ const routes = [{
         component: () =>
             import ( /* webpackChunkName: "about" */ "../views/About.vue"),
     },
-    // {
-    //     path: "/checkout",
-    //     name: "checkout",
-    //     // route level code-splitting
-    //     // this generates a separate chunk (about.[hash].js) for this route
-    //     // which is lazy-loaded when the route is visited.
-    //     component: () =>
-    //         import ( /* webpackChunkName: "about" */ "../views/Checkout.vue")
-    // },
+    {
+        path: "/checkout",
+        name: "checkout",
+        // route level code-splitting
+        // this generates a separate chunk (about.[hash].js) for this route
+        // which is lazy-loaded when the route is visited.
+        component: () =>
+            import ( /* webpackChunkName: "about" */ "../views/Checkout.vue")
+    },
 ];
 
 const router = createRouter({
--- src/views/Checkout.vue
+++ src/views/Checkout.vue
@@ -1,11 +1,8 @@
 <template>
   <div class="chekout">
-       <Navbar></Navbar>
-
+    <Navbar></Navbar>
         <div class="container mt-5 pt-5">
-
             <div class="row">
-
                 <div class="col-md-8">
                     <h4 class="py-4">Checkout page</h4>
                      <ul>
@@ -13,44 +10,33 @@
                         <img :src="item.productImage" width="80px" class="align-self-center mr-3" alt="">
                         <div class="media-body">
                             <h5 class="mt-0">{{item.productName}}
-
                                 <span class='float-right' @click="$store.commit('removeFromCart',item)">X</span>
-
                             </h5>
                             <p class="mt-0">{{ formatPrice(item.productPrice)}}</p>
                             <p class="mt-0">Quantity : {{item.productQuantity }}</p>
                         </div>
                         </li>
-
                     </ul>
                 </div>
                 <div class="col-md-4">
                     <p>
                         Total Price : {{ formatPrice(this.$store.getters.totalPrice ) }}
                     </p>
-
-                   
-
                     <Card class='stripe-card'
                         :class='{ complete }'
                         stripe='pk_test_XXXXXXXXXXXXXXXXXXXXXXXX'
                         :options='stripeOptions'
                         @change='complete = $event.complete'
                         />
-
                         <button class='pay-with-stripe btn btn-primary mt-4' @click='pay' :disabled='!complete'>Pay with credit card</button>
-                    
                 </div>
             </div>
-
         </div>
   </div>
 </template>
-
 <script>
-
 import { Card, createToken } from 'vue-stripe-elements-plus';
-
+  import Navbar from "@/components/Navbar.vue";
 
 export default {
     data () {
@@ -61,9 +47,7 @@ export default {
       }
     }
   },
-
-  components: { Card },
-
+  components: { Card,Navbar },
   methods: {
       formatPrice(price) {
                 return "$" + price;
@@ -79,9 +63,6 @@ export default {
   }
 }
 </script>
-
-
-
 <style>
 /**
  * The CSS shown here will not be introduced in the Quickstart guide, but shows
@@ -89,28 +70,21 @@ export default {
  */
 .StripeElement {
   box-sizing: border-box;
-
   height: 40px;
-
   padding: 10px 12px;
-
   border: 1px solid transparent;
   border-radius: 4px;
   background-color: white;
-
   box-shadow: 0 1px 3px 0 #e6ebf1;
   -webkit-transition: box-shadow 150ms ease;
   transition: box-shadow 150ms ease;
 }
-
 .StripeElement--focus {
   box-shadow: 0 1px 3px 0 #cfd7df;
 }
-
 .StripeElement--invalid {
   border-color: #fa755a;
 }
-
 .StripeElement--webkit-autofill {
   background-color: #fefde5 !important;
 }
